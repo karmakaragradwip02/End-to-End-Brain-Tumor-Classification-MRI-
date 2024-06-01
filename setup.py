c50a87492c18c50e7171eaad756335932af10b9a
Agradwip Karmakar <karmakaragradwip02@gmail.com>
2024-06-01 10:12:29 +0530

    setup4

--- setup.py
+++ setup.py
@@ -5,9 +5,9 @@ with open("README.md", "r", encoding="utf-8") as f:
 
 __version__ = "0.1.0"
 
-REPO_NAME = "End-to-End-Brain-Tumor-Classification-MRI-"
-AUTHOR_USER_NAME = "karmakaragradwip02" 
-SRC_REPO = "Brain_Tumor_Classification(MRI)"
+REPO_NAME = "End-to-End-Brain-Tumor-Classification-MRI"
+AUTHOR_USER_NAME = "karmakaragradwip02"
+SRC_REPO = "Brain_Tumor_Classification_MRI"
 AUTHOR_EMAIL = "karmakaragradwip02@gmail.com"
 
 setuptools.setup(
@@ -15,13 +15,13 @@ setuptools.setup(
     version=__version__,
     author=AUTHOR_USER_NAME,
     author_email=AUTHOR_EMAIL,
-    description="A small python package for CNN app",
+    description="CNN APP FOR BRAIN TUMOR CLASSIFICATION (MRI)",
     long_description=long_description,
-    long_description_content="text/markdown",
+    long_description_content_type="text/markdown",
     url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
     project_urls={
         "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
     },
     package_dir={"": "src"},
-    packages=setuptools.find_packages(where="src")
+    packages=setuptools.find_packages(where="src"),
 )
