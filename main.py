f6244babe184b5b2bd5b99795fa7c0ef0e9286e6
Agradwip Karmakar <karmakaragradwip02@gmail.com>
2024-06-04 10:20:07 +0530

    data ingestion added

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
-from src.Brain_Tumor_Classification_MRI import logger
+from Brain_Tumor_Classification_MRI import logger
+from Brain_Tumor_Classification_MRI.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 
-logger.info("Welcome to Brain Tumor MRI Classifier log2")
+STAGE_NAME = "DATA INGESTION STAGE"
+
+try: 
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    obj = DataIngestionTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<")
+except Exception as e:
+    logger.exception(e)
+    raise e
---
+++ src/Brain_Tumor_Classification_MRI/pipeline/stage_01_data_ingestion.py
@@ -0,0 +1,27 @@
+from Brain_Tumor_Classification_MRI.config.configuration import ConfigurationManager
+from Brain_Tumor_Classification_MRI.components.data_ingestion import DataIngestion
+from Brain_Tumor_Classification_MRI import logger
+
+STAGE_NAME = "DATA INGESTION STAGE"
+
+class DataIngestionTrainingPipeline:
+    def __init__(self):
+        pass
+
+    def main(self):
+        config = ConfigurationManager()
+        data_ingestion_config = config.get_data_ingestion_config()
+        data_ingestion = DataIngestion(config=data_ingestion_config)
+        data_ingestion.download_file()
+        data_ingestion.extract_zip_file()
+    
+
+if __name__ == '__name__':
+    try: 
+        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+        obj = DataIngestionTrainingPipeline()
+        obj.main()
+        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<")
+    except Exception as e:
+        logger.exception(e)
+        raise e
